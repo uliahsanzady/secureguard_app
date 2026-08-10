829903ed28d1b83ca20aa22f94c65a03d81b3a4f
Uli Ahsan Zadi <uliahsanzady@gmail.com>
2026-08-10 12:18:56 +0700

    [FIX] - Memperbaiki struktur folder dan error kompilasi

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1A237E);
+  static const Color primaryLight = Color(0xFF3949AB);
+  static const Color primaryDark = Color(0xFF0D1442);
+  static const Color accent = Color(0xFFFF6B35);
+  static const Color success = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFFC107);
+  static const Color danger = Color(0xFFF44336);
+  static const Color greyLight = Color(0xFFF5F5F5);
+  static const Color greyMedium = Color(0xFFE0E0E0);
+  static const Color greyText = Color(0xFF757575);
+  static const Color greyDark = Color(0xFF424242);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
